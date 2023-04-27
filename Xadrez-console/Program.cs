8ae41247d94dee34d451bdb9fdca741b38e5940e
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-04-27 10:04:43 -0300

    Criando a classe posição xadrez

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -8,21 +8,10 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-
-                Tela.ImprimirTabuleiro(tab);
-
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.ToPosicao());
         }
     }
 }
---
+++ Xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using Xadrez_console.tabuleiro;
+
+namespace Xadrez_console.xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao() // fazendo a conta de 8 - a linha a gente chega no valor da matriz  pra conseguir converter no numero
+        {                          // representado no board de xadres, o mesmo serve para coluna pois o 'a' é considerado um numero inteiro
+            return new Posicao(8 - Linha, Coluna - 'a'); 
+        }
+
+        public override string ToString()
+        {
+            return $"{Coluna}{Linha}";
+        }
+    }
+}
